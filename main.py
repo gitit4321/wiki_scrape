2b08ade46e5beb6cb053d8488b51c02c8565ba12
gitit4321 <gitit4321@gmail.com>
2021-07-21 10:55:20 -0400

    added cors clause

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from wiki_scrape import get_wiki_summary
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root(query: str):
